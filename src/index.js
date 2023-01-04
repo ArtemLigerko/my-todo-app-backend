5ff5cc357abdc19165f18721dc2d498d95487bee
Artem Ligerko <artmarket81@gmail.com>
2023-01-04 19:22:38 +0200

    fix: fixed CORS error

--- src/index.js
+++ src/index.js
@@ -1,27 +1,45 @@
 import express from "express";
 import mongoose from "mongoose";
-import router from "./todo.routes.js";
 import * as dotenv from "dotenv";
+import cors from "cors";
+
+import router from "./todo.routes.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
-const DB_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT;
+const DB_URL = process.env.MONGO_URI;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
+
 app.get("/", (req, res) => {
   res.status(200).json("Server working");
+});
+app.get("/api/todos", (req, res) => {
+  res.status(200).json();
 });
 
 mongoose.set("strictQuery", true);
 
 const startApp = async () => {
+  const db = mongoose.connection;
+
   try {
-    await mongoose.connect(DB_URL);
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    mongoose.connect(DB_URL);
+
+    //mongo db connection succeed check
+    db.once("open", (_) => {
+      console.log("Database connected:", DB_URL);
+    });
+    db.on("error", (err) => {
+      console.error("connection error:", DB_URL);
+    });
+
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }
